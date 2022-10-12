2a62541b5251912af8d7e364d518b508874bf4fa
KnightLeo <knightleo602@gmail.com>
2022-10-12 18:22:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
 
-url_livros = ["https://www.gutenberg.org/files/2701/2701-h/2701-h.htm",
-              "https://www.gutenberg.org/files/2097/2097-h/2097-h.htm",
-              "https://www.gutenberg.org/files/68283/68283-h/68283-h.htm",
-              "https://www.gutenberg.org/files/84/84-h/84-h.htm",
-              "https://www.gutenberg.org/files/345/345-h/345-h.htm"]
+url_livros = ["https://www.sas.com/en_us/insights/analytics/what-is-natural-language-processing-nlp.html",
+              "https://hbr.org/2022/04/the-power-of-natural-language-processing",
+              "https://monkeylearn.com/natural-language-processing/",
+              "https://www.tableau.com/learn/articles/natural-language-processing-examples",
+              "https://towardsdatascience.com/your-guide-to-natural-language-processing-nlp-48ea2511f6e1"]
 
 if __name__ == '__main__':
-
     sentencas = []
-    for url_i in range(len(url_livros)):
-        res = requests.get(url_livros[url_i])
+    for j in range(len(url_livros)):
+        res = requests.get(url_livros[j])
         soup = BeautifulSoup(res.text, "html5lib")
-        count = 0
         words = soup.get_text().split(" ")
         sentencas.append([])
-        ok = False
         sentenca = ""
         for i in range(len(words)):
             if words[i].endswith("."):
-                sentenca += words[i]
-                sentencas[url_i].append(sentenca)
+                sentenca += words[j]
+                sentencas[j].append(sentenca)
                 sentenca = ""
-            elif not(words == " "):
+            elif not(words[i].isspace()):
                 sentenca += words[i].replace("\n", "") + " "
-    print("fim")
-    print("sentencas criadas: ", end="")
-    print(sentencas)
