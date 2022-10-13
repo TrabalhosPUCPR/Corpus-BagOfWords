47b3b36565ad8c145c2264adedc95f997b293367
KnightLeo <80933008+Knightleo602@users.noreply.github.com>
2022-10-13 08:05:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+""" LEONARDO MATTHEW KNIGHT
+Sua  tarefa  será  transformar  um  conjunto  de  5  sites,  sobre  o  tema  de  processamento  de 
+linguagem natural em um conjunto de cinco listas distintas de sentenças. Ou seja, você fará uma função 
+que, usando a biblioteca Beautifull Soap, faça a requisição de uma url, e extrai todas as sentenças desta 
+url. Duas condições são importantes:  
+a) A página web (url) deve apontar para uma página web em inglês contendo, não menos que 
+1000 palavras.  
+b) O texto desta página deverá ser transformado em um array de senteças.  
+ 
+Para separar as sentenças você pode usar os sinais de pontuação ou as funções da biblibioteca 
+Spacy. 
+"""
+
 from bs4 import BeautifulSoup
 import requests
 
