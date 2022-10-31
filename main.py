31d7252820a71c16d37032b37811efa5adb3ca25
KnightLeo <knightleo602@gmail.com>
2022-10-31 15:40:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,44 @@
 """ LEONARDO MATTHEW KNIGHT
-Sua  tarefa  será  transformar  um  conjunto  de  5  sites,  sobre  o  tema  de  processamento  de 
-linguagem natural em um conjunto de cinco listas distintas de sentenças. Ou seja, você fará uma função 
-que, usando a biblioteca Beautifull Soap, faça a requisição de uma url, e extrai todas as sentenças desta 
-url. Duas condições são importantes:  
-a) A página web (url) deve apontar para uma página web em inglês contendo, não menos que 
-1000 palavras.  
-b) O texto desta página deverá ser transformado em um array de senteças.  
- 
-Para separar as sentenças você pode usar os sinais de pontuação ou as funções da biblibioteca 
-Spacy. 
+Sua tarefa será gerar a matriz termo documento, dos documentos recuperados da internet e
+imprimir esta matriz na tela. Para tanto:
+a) Considere que todas as listas de sentenças devem ser transformadas em listas de vetores,
+onde cada item será uma das palavras da sentença.
+b) Todos os vetores devem ser unidos em um corpus único formando uma lista de vetores,
+onde cada item será um lexema.
+c) Este único corpus será usado para gerar o vocabulário.
+d) O resultado esperado será uma matriz termo documento criada a partir da aplicação da
+técnica bag of Words em todo o corpus.
 """
 
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
 
-url_livros = ["https://www.sas.com/en_us/insights/analytics/what-is-natural-language-processing-nlp.html",
-              "https://hbr.org/2022/04/the-power-of-natural-language-processing",
-              "https://monkeylearn.com/natural-language-processing/",
-              "https://www.tableau.com/learn/articles/natural-language-processing-examples",
-              "https://towardsdatascience.com/your-guide-to-natural-language-processing-nlp-48ea2511f6e1"]
+urls = ["https://www.sas.com/en_us/insights/analytics/what-is-natural-language-processing-nlp.html",
+        "https://hbr.org/2022/04/the-power-of-natural-language-processing",
+        "https://monkeylearn.com/natural-language-processing/",
+        "https://www.tableau.com/learn/articles/natural-language-processing-examples",
+        "https://towardsdatascience.com/your-guide-to-natural-language-processing-nlp-48ea2511f6e1"]
+vec = CountVectorizer()
 
 if __name__ == '__main__':
     sentencas = []
-    for j in range(len(url_livros)):
-        res = requests.get(url_livros[j])
+    for j in range(len(urls)):
+        res = requests.get(urls[j])
         soup = BeautifulSoup(res.text, "html5lib")
         words = soup.get_text().split(" ")
         sentencas.append([])
         sentenca = ""
         for i in range(len(words)):
-            if words[i].endswith("."):
-                sentenca += words[j]
-                sentencas[j].append(sentenca)
-                sentenca = ""
-            elif not(words[i].isspace()):
-                sentenca += words[i].replace("\n", "") + " "
+            words[i] = words[i].replace("\n", "")
+            if not (words[i].isalpha()) and not (words[i].isdigit()):
+                if not (sentenca == ""):
+                    sentencas[j].append(sentenca)
+                    sentenca = ""
+            elif not (words[i].isspace()):
+                sentenca += words[i] + " "
+        X = vec.fit_transform(sentencas[len(sentencas) - 1])
+        df = pd.DataFrame(X.toarray(), columns=vec.get_feature_names_out())
+        df.head()
+        print(df)
