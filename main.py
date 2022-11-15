57f4bc3c47652c06a0f913257979bb60da77df5e
KnightLeo <knightleo602@gmail.com>
2022-11-14 21:07:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,50 @@
 """ LEONARDO MATTHEW KNIGHT
-Sua tarefa será gerar a matriz termo documento, dos documentos recuperados da internet e
-imprimir esta matriz na tela. Para tanto:
-a) Considere que todas as listas de sentenças devem ser transformadas em listas de vetores,
-onde cada item será uma das palavras da sentença.
-b) Todos os vetores devem ser unidos em um corpus único formando uma lista de vetores,
-onde cada item será um lexema.
-c) Este único corpus será usado para gerar o vocabulário.
-d) O resultado esperado será uma matriz termo documento criada a partir da aplicação da
-técnica bag of Words em todo o corpus.
+Sua  tarefa  será  transformar  um  conjunto  de  5  sites,  sobre  o  tema  de  processamento  de
+linguagem natural em um conjunto de cinco listas distintas de sentenças. Ou seja, você fará uma função
+que, usando a biblioteca Beautifull Soap, faça a requisição de uma url, e extrai todas as sentenças desta
+url. Duas condições são importantes:
+a) A página web (url) deve apontar para uma página web em inglês contendo, não menos que
+1000 palavras.
+b) O texto desta página deverá ser transformado em um array de senteças.
+
+Para separar as sentenças você pode usar os sinais de pontuação ou as funções da biblibioteca
+Spacy.
 """
 
 from bs4 import BeautifulSoup
 import requests
-import pandas as pd
-from sklearn.feature_extraction.text import CountVectorizer
+import re
 
 urls = ["https://www.sas.com/en_us/insights/analytics/what-is-natural-language-processing-nlp.html",
         "https://hbr.org/2022/04/the-power-of-natural-language-processing",
         "https://monkeylearn.com/natural-language-processing/",
         "https://www.tableau.com/learn/articles/natural-language-processing-examples",
         "https://towardsdatascience.com/your-guide-to-natural-language-processing-nlp-48ea2511f6e1"]
-vec = CountVectorizer()
+pageSentences = []
+
+
+def separatetext(t):
+    t = t.replace("\n", "")
+    # https://stackoverflow.com/a/25736082 peguei esse regex pra separar em sentenca daqui
+    sentences = re.split(r"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s", t)
+    sentences = [sent.strip(" ") for sent in sentences]
+    return sentences
+
+
+def corpus():
+    n = 0
+    for url in urls:
+        html = requests.get(url)
+        bs = BeautifulSoup(html.text, "html.parser")
+        text = bs.find_all("p")
+        pageSentences.append([])
+        for te in text:
+            for result in separatetext(te.get_text()):
+                if result != '':
+                    pageSentences[n].append(result)
+        print(pageSentences[n])
+        n += 1
+
 
 if __name__ == '__main__':
-    sentencas = []
-    for j in range(len(urls)):
-        res = requests.get(urls[j])
-        soup = BeautifulSoup(res.text, "html5lib")
-        words = soup.get_text().split(" ")
-        sentencas.append([])
-        sentenca = ""
-        for i in range(len(words)):
-            words[i] = words[i].replace("\n", "")
-            if not (words[i].isalpha()) and not (words[i].isdigit()):
-                if not (sentenca == ""):
-                    sentencas[j].append(sentenca)
-                    sentenca = ""
-            elif not (words[i].isspace()):
-                sentenca += words[i] + " "
-        X = vec.fit_transform(sentencas[len(sentencas) - 1])
-        df = pd.DataFrame(X.toarray(), columns=vec.get_feature_names_out())
-        df.head()
-        print(df)
+    corpus()
