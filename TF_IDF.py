5b32a4b7f715a09f41f70c8efd415a86af8b4f08
KnightLeo <80933008+Knightleo602@users.noreply.github.com>
2022-11-16 22:02:40 -0300

    Update TF_IDF.py

--- TF_IDF.py
+++ TF_IDF.py
@@ -1,3 +1,20 @@
+""" LEONARDO MATTHEW KNIGHT
+Sua tarefa será gerar a matriz termo-documento usando TF-IDF por meio da aplicação das 
+fórmulas  TF-IDF  na  matriz  termo-documento  criada  com  a  utilização  do  algoritmo  Bag of 
+Words. Sobre o Corpus que recuperamos anteriormente. O entregável desta tarefa é uma 
+matriz termo-documento onde a primeira linha são os termos e as linhas subsequentes são 
+os vetores calculados com o TF-IDF. 
+2. Sua tarefa será gerar uma matriz de distância, computando o cosseno do ângulo entre todos 
+os vetores que encontramos usando o tf-idf. Para isso use a seguinte fórmula para o cálculo 
+do  cosseno  use  a  fórmula  apresentada  em  Word2Vector  (frankalcantara.com) 
+(https://frankalcantara.com/Aulas/Nlp/out/Aula4.html#/0/4/2)  e  apresentada  na  figura  a 
+seguir:  
+
+O resultado deste trabalho será uma matriz que relaciona cada um dos vetores já calculados 
+com todos os outros vetores disponíveis na matriz termo-documento mostrando a distância 
+entre cada um destes vetores. 
+"""
+
 import sklearn.feature_extraction.text as sk
 from bagofwords import countVector
 from corpus import pageSentences
